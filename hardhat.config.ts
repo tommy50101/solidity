a23b28d1b802879214afb016aeb70d5e7a087ce7
tommy50101 <tommy50101@DESKTOP-524H42K.localdomain>
2022-10-16 00:23:50 +0800

    Set hardhat.config.ts with Georli network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    Georli: {
+      url: "https://goerli.infura.io/v3/84a99a188f8e4aaab60c45f9955c5d6b",
+      accounts: ['d2cf733a887c530e705fb92fbb69a98b5ec0f469e782c9f5746aa61ea7d331ab']
+    }
+  }
 };
 
 export default config;
