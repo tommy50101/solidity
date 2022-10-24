17dedf5612955d410ae1cdb7c07e442952841e30
tommy50101 <tommy50101@DESKTOP-524H42K.localdomain>
2022-10-25 01:37:04 +0800

    init: init master

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-etherscan';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-  networks: {
-    Georli: {
-      url: "https://goerli.infura.io/v3/84a99a188f8e4aaab60c45f9955c5d6b",
-      accounts: ['d2cf733a887c530e705fb92fbb69a98b5ec0f469e782c9f5746aa61ea7d331ab']
-    }
-  }
+    solidity: '0.8.17',
+    networks: {
+        Goerli: {
+            url: 'https://goerli.infura.io/v3/84a99a188f8e4aaab60c45f9955c5d6b',
+            accounts: ['d2cf733a887c530e705fb92fbb69a98b5ec0f469e782c9f5746aa61ea7d331ab'],
+        },
+    },
+    etherscan: {
+        apiKey: {
+            goerli: 'G94NGYIUVNSKHZWN1KGZU9VB6KBQYCKE17',
+        },
+    },
 };
 
 export default config;
