b49bd1b63ea38658ae02b8c7e1b39c6654322649
Tommy <tommy50101@gmail.com>
2022-10-29 02:26:16 +0800

    fix: Fix typo

--- contracts/TestErc20.sol
+++ contracts/TestErc20.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestErc20 is ERC20 {
-    constructor(uint256 supply, string memory name, string memmory symbol) ERC20("TestErc20", "TE2") {
+    constructor(uint256 supply, string memory name, string memory symbol) ERC20("TestErc20", "TE2") {
         _mint(msg.sender, supply);
     }
 }
